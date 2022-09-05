d58b05786623a8ad938179d21f9ee2f04b949003
M. Badar Wildani <badar.wildanie@forstok.com>
2022-09-05 17:49:48 +0700

    add: user model

---
+++ models/user.model.js
@@ -0,0 +1,40 @@
+const mongo = require('../database/mongo')
+
+class UserModel {
+
+    constructor() {
+        this.collection = mongo.db.collection('user')
+    }
+
+    create = async (data) => {
+        const data = await this.collection.insertOne(data)
+        return data
+    }
+
+    update = async (data, query) => {
+        const data = await this.collection.updateOne(query, update)
+        return data
+    }
+
+    delete = async (query) => {
+        const result = await this.collection.deleteOne(query)
+        return result
+    }
+
+    createMany = async (data) => {
+        const data = await this.collection.insertMany(data)
+        return data
+    }
+
+    updateMany = async (data, query) => {
+        const data = await this.collection.updateMany(query, update)
+        return data
+    }
+
+    deleteMany = async (query) => {
+        const result = await this.collection.deleteMany(query)
+        return result
+    }
+}
+
+module.exports = UserModel
