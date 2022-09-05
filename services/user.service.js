63e579decfc8cfc72c41d5552593558f843d0416
M. Badar Wildani <badar.wildanie@forstok.com>
2022-09-05 23:52:30 +0700

    fix: returned inserted ID when success

--- services/user.service.js
+++ services/user.service.js
@@ -1,18 +1,19 @@
-
-
-const UserEntity = require('../entities/user.entity')
-const UserModel = require('../models/user.model')
+const UserEntity = require("../entities/user.entity")
+const UserModel = require("../models/user.model")
 
 const userModel = new UserModel()
 
-
 /**
- * 
- * @param {UserEntity} userEntity 
+ *
+ * @param {UserEntity} userEntity
  */
 const create = async (userEntity) => {
     const user = await userModel.create(userEntity)
-    console.log('Success creating user, inserted id: ' + user.insertedId)
+    console.log("Success creating user, inserted id: " + user.insertedId)
+
+    return {
+        insertedId: user.insertedId,
+    }
 }
 
 module.exports = { create }
