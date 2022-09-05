b0d463da0966ea9ef08473bad3a8a4dd5c99c249
M. Badar Wildani <badar.wildanie@forstok.com>
2022-09-05 18:09:50 +0700

    add: user entity

---
+++ entities/user.entity.js
@@ -0,0 +1,62 @@
+
+
+class User {
+    
+    constructor(object = false) {
+        if (object) {
+            const { name, username, email, address, skill } = object
+            this.name = name
+            this.username = username
+            this.email = email
+            this.address = address
+            this.skill = skill
+        }
+    }
+
+    // name
+    setName = (name) => {
+        this.name = name
+        return this
+    }
+    getName = () => {
+        return this.name
+    }
+
+    // username
+    setUsername = (username) => {
+        this.username = username
+        return this
+    }
+    getUsername = () => {
+        return this.username
+    }
+
+    // email
+    setEmail = (email) => {
+        this.email = email
+        return this
+    }
+    getEmail = () => {
+        return this.email
+    }
+
+    // address
+    setAddress = (address) => {
+        this.address = address
+        return this
+    }
+    getAddress = () => {
+        return this.address
+    }
+
+    // Skill
+    setSkill = (skill) => {
+        this.skill = skill
+        return this
+    }
+    getSkill = () => {
+        return this.skill
+    }
+}
+
+module.exports = User
