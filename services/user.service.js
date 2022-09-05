ea266155e77d3a33d8cf906a49c4039c3b2e3363
M. Badar Wildani <badar.wildanie@forstok.com>
2022-09-05 18:33:12 +0700

    add: user service

---
+++ services/user.service.js
@@ -0,0 +1,18 @@
+
+
+const UserEntity = require('../entities/user.entity')
+const UserModel = require('../models/user.model')
+
+const userModel = new UserModel()
+
+
+/**
+ * 
+ * @param {UserEntity} userEntity 
+ */
+const create = async (userEntity) => {
+    const user = await userModel.create(userEntity)
+    console.log('Success creating user, inserted id: ' + user.insertedId)
+}
+
+module.exports = { create }
