a32abae859e33257aed3cba44dfe0d861c31cc70
M. Badar Wildani <badar.wildanie@forstok.com>
2022-09-06 18:33:06 +0700

    doc: added jsdoc

--- models/user.model.js
+++ models/user.model.js
@@ -1,26 +1,53 @@
 const mongo = require("../database/mongo")
 
+const UserEntity = require("../entities/user.entity")
+
 const objectHelper = require("../app/http/helpers/object.helper")
 class UserModel {
     constructor() {
         this.collection = mongo.db.collection("user")
     }
 
+    /**
+     * Find All user
+     *
+     * @param {Object} query query filter object to select
+     * @returns
+     */
     findAll = async (query = {}) => {
         const tx = this.collection.find(query)
         return tx
     }
 
+    /**
+     * Find user
+     *
+     * @param {Object} query query filter object to select
+     * @returns
+     */
     find = async (query) => {
         const data = await this.collection.findOne(query)
         return data
     }
 
+    /**
+     * Create user
+     *
+     * @param {UserEntity} data
+     * @returns
+     */
     create = async (data) => {
         const tx = await this.collection.insertOne(data)
         return tx
     }
 
+    /**
+     * Update user
+     *
+     * @param {UserEntity} data
+     * @param {Object} query
+     * @returns
+     */
     update = async (data, query) => {
         const sanitized = objectHelper.compactNested(data)
 
@@ -28,21 +55,46 @@ class UserModel {
         return tx
     }
 
+    /**
+     * Delete user
+     *
+     * @param {Object} query
+     * @returns
+     */
     delete = async (query) => {
         const result = await this.collection.deleteOne(query)
         return result
     }
 
+    /**
+     * Create multiple user resource
+     *
+     * @param {Array.<UserEntity>} data
+     * @returns
+     */
     createMany = async (data) => {
         const tx = await this.collection.insertMany(data)
         return tx
     }
 
+    /**
+     * Update multiple user resource
+     *
+     * @param {UserEntity} data
+     * @param {Object} query
+     * @returns
+     */
     updateMany = async (data, query) => {
         const tx = await this.collection.updateMany(query, update)
         return tx
     }
 
+    /**
+     * Delete multiple user resource
+     *
+     * @param {Object} query
+     * @returns
+     */
     deleteMany = async (query) => {
         const result = await this.collection.deleteMany(query)
         return result
