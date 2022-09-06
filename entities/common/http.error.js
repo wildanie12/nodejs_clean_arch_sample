69b65ca3d15b09380cbb10e1d2c5c9cc0ed99978
M. Badar Wildani <badar.wildanie@forstok.com>
2022-09-06 14:56:23 +0700

    add: custom error entity

---
+++ entities/common/http.error.js
@@ -0,0 +1,16 @@
+class HttpError extends Error {
+    /**
+     *
+     * @param {string} message HTTP Error message
+     * @param {number} attribute.code HTTP Error code
+     * @param {string} attribute.filename HTTP Error code
+     */
+    constructor(message, attribute = { code: 500, filename: "" }) {
+        super(message)
+        this.name = "ERROR_HTTP"
+        this.code = attribute.code
+        this.filename = attribute.filename
+    }
+}
+
+module.exports = HttpError
